c3c845156e95ef7776aa96dc33b011c58824e17e
shivani26shinde <shindeshivani26@gmail.com>
2017-11-01 22:41:45 -0500

    1st javascript program

---
+++ jsCode1.js
@@ -0,0 +1,17 @@
+//JavaaScript Code 1
+
+function processData(input) {
+    input = "Hello World!";
+    console.log(input);
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
