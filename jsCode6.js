fc0c25c8772d7ff09805aae70e3b9f3239cbddf7
shivani26shinde <shindeshivani26@gmail.com>
2017-11-08 23:32:35 -0600

    Completed..

---
+++ jsCode6.js
@@ -0,0 +1,33 @@
+// Find the day
+
+function findDay(myDate) {
+    // Return day for date myDate(MM/DD/YYYY)
+    // Note that myDate contains the date in string format
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var day = new Date(myDate);
+    var n = weekday[day.getDay()];
+    console.log(n);
+} 
+
+// tail starts here
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+    var dates = _input.split('\n');
+
+    for (var i = 0; i < dates.length - 1; i++) {
+        findDay(dates[i]);
+    }
+});
